dff6cb8f301aa608f780886fc0ba2783221b7cab
elenapetra <elena_petrasco@epam.com>
2024-02-27 13:28:01 +0200

    Updated files for Docker compatibility and local environment support

--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,15 @@
-# Use an official Node.js runtime as a parent image
 FROM node:18-alpine
 
-# Set the working directory
 WORKDIR /app
 
-# Copy package.json and package-lock.json to the container
 COPY package*.json ./
 
-# Install dependencies
 RUN npm install
 
-
-# Copy the rest of the application code to the container
 COPY . .
 
-# Expose port 3000
 EXPOSE 8000
 
-# Start the app
+ENV DOCKERIZED=true
+
 CMD [ "npm", "start" ]
--- src/db.ts
+++ src/db.ts
@@ -1,9 +1,14 @@
 import mongoose from "mongoose";
 import { logger } from "./utils/logger";
 
+const isDockerized = process.env.DOCKERIZED === "true";
+
+const URI = isDockerized
+  ? "mongodb://mongo:27017/shopDB"
+  : "mongodb://127.0.0.1:27017/shopDB";
+
 const connectDB = async () => {
   try {
-    const URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/shopDB";
     await mongoose.connect(URI);
     logger.info("Connected to MongoDB");
   } catch (error: any) {
